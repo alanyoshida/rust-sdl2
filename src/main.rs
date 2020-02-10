0f08128dcebb1eb76e45224d463c6c2fe1a166a2
Alan Yoshida <alanyoshida@msn.com>
2020-02-10 18:28:37 -0300

    Player is moving

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
-use sdl2::rect::{Point, Rect};
-
+use sdl2::rect::Rect;
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -17,32 +16,66 @@ pub fn main() {
         .unwrap();
  
     let mut canvas = window.into_canvas().build().unwrap();
- 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+
+    // This handles the events from keyboard
     let mut event_pump = sdl_context.event_pump().unwrap();
+
+    struct Player {
+        x: i32,
+        y: i32,
+        width: u32,
+        height: u32
+    };
+    let mut player = Player {
+        x: 50,
+        y: 50,
+        width: 20,
+        height: 20
+    };
 
     // GAME LOOP
     'running: loop {
-        
+        // CLEAR WINDOW EACH FRAME
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
 
+        // Draw player
         canvas.set_draw_color(Color::RGB(255, 210, 0));
-        canvas.fill_rect(Rect::new(10, 10, 20, 20));
+        canvas
+        .fill_rect(Rect::new(player.x, player.y, player.width, player.height))
+        .unwrap_or_else(|error| {
+            panic!("{:?}", error);
+        });
 
+        // Capture events from keyboard
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
                 },
+                Event::KeyDown { keycode: Some(Keycode::Up), ..} => {
+                    println!("Up");
+                    player.y -= 10;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Down), ..} => {
+                    println!("Down");
+                    player.y += 10;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Left), ..} => {
+                    println!("Left");
+                    player.x -= 10;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Right), ..} => {
+                    println!("Right");
+                    player.x += 10;
+                },
                 _ => {}
             }
         }
         // The rest of the game loop goes here...
 
+        // DRAW CANVAS ON WINDOW
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
